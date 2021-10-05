3cc3f9b921fecdc9145a916e9f81a86e4b90900b
adam thibodeaux <athib27@gmail.com>
2021-10-05 11:44:20 -0400

    updated gitignore and updated server to include express typing

--- client/server/server.ts
+++ client/server/server.ts
@@ -2,4 +2,10 @@ import * as express from 'express';
 
 const app = express();
 
+app.use(
+  '/',
+  (req: express.Request, res: express.Response, next: express.NextFunction) => {
+    res.sendStatus(201);
+  }
+);
 app.listen(3000, () => console.log('listening on port 3000 :)'));
