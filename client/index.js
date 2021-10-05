c9534a3366c83ed0b1589ef69fb230e17cc9ed16
adam thibodeaux <athib27@gmail.com>
2021-10-05 12:24:06 -0400

    updated package.json script to force esModuleInterop for TSC, also changed import statements and installed typing for electron/expess

--- client/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var React = require("react");
-var react_dom_1 = require("react-dom");
-var App_1 = require("./components/App");
-(0, react_dom_1.render)(React.createElement(App_1["default"], null), document.querySelector('#main'));
