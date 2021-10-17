78b01ddc0536b300709907190f93a2ff391e5328
adam thibodeaux <athib27@gmail.com>
2021-10-17 10:10:55 -0400

    added extra try/catch blocks in sqlite connections to improve error handling

--- client/server/kafkaController.ts
+++ client/server/kafkaController.ts
@@ -111,131 +111,141 @@ const controller: controller = {
   },
   //after verifying broker exists using kafkajs admin, adds each topic and it's partitions and respective offsets to sqlite
   createTable: async function (req, res, next) {
-    const { bootstrap } = req.body;
-    //if there is no server given, we send an error page
-    if (!bootstrap.length) res.sendStatus(403);
-    //sanitizing for sql
-    const bootstrapSanitized = bootstrap.replace(':', '_');
-    const instance = new kafka.Kafka({
-      clientId: 'testing2',
-      brokers: [`${bootstrap}`], //must be unsanitized form
-    });
-    const admin = instance.admin();
-    admin.connect();
-    //fetching topics for that broker
-    const results = await admin.listTopics();
-    //creating an empty object that holds offsets for each topic
-    const offsets: {
-      [key: string]: (kafka.SeekEntry & { high: string; low: string })[];
-    } = {};
-    //fetching all offsets for each partition and saving that in the offsets object
-    Promise.all(
-      results.map((el) => {
-        return admin
-          .fetchTopicOffsets(el)
-          .then((result) => (offsets[el] = result));
-      })
-    ).then(() => {
-      //getting max number of partitions for entire broker (must have for SQL, as table columns cannot be altered after creating)
-      //so each topic will have the same number of partitions in SQL db, but null values in sqldb indicate the partition does not exist on that topic in kafka
-      // topic1 w/ 3 partitions (max for broker) -> {topic: topic1, partition_0: 5, partition_1: 2, partition_3: 0}
-      // topic2 w/ 1 partition (< max for broker) -> {topic: topic2, partition_0: 10, partition_1: null, partition_3: null}
-      let maxPartitions = 0;
-      Object.keys(offsets).forEach((el) => {
-        offsets[el].forEach((el2) => {
-          if (el2.partition > maxPartitions) maxPartitions = el2.partition;
-        });
+    try {
+      const { bootstrap } = req.body;
+      //if there is no server given, we send an error page
+      if (!bootstrap.length) res.sendStatus(403);
+      //sanitizing for sql
+      const bootstrapSanitized = bootstrap.replace(':', '_');
+      const instance = new kafka.Kafka({
+        clientId: 'testing2',
+        brokers: [`${bootstrap}`], //must be unsanitized form
       });
-      //creating partition string for SQL query, creates partition_X column in db
-      let partitionString = '';
-      for (let i = 0; i <= maxPartitions; i++) {
-        if (i < maxPartitions) partitionString += `partition_${i} int,`;
-        //below is for last value, which !!!cannot!!! have comma after it
-        else partitionString += `partition_${i} int`;
-      }
-      open({ filename: '/tmp/database.db', driver: sqlite3.Database })
-        .then((db) => {
-          db.exec(
-            `CREATE TABLE ${bootstrapSanitized} (topic varchar(255), ${partitionString});`
-          );
-          return db;
+      const admin = instance.admin();
+      admin.connect();
+      //fetching topics for that broker
+      const results = await admin.listTopics();
+      //creating an empty object that holds offsets for each topic
+      const offsets: {
+        [key: string]: (kafka.SeekEntry & { high: string; low: string })[];
+      } = {};
+      //fetching all offsets for each partition and saving that in the offsets object
+      Promise.all(
+        results.map((el) => {
+          return admin
+            .fetchTopicOffsets(el)
+            .then((result) => (offsets[el] = result));
         })
-        .then((db) => {
-          Object.keys(offsets).forEach((el) => {
-            //below is for generic column names e.g. topic, partition_0, partition_1...
-            let colString = 'topic, ';
-            //below is the actual values for those columns above(!!topic must be in quotes!!) e.g. 'intial_report', 10, 7
-            let valueString = `'${el}', `;
-            offsets[el].forEach((el2) => {
-              valueString += `'${el2.offset}',`;
-              colString += `partition_${el2.partition},`;
-            });
-            valueString = valueString.slice(0, valueString.length - 1);
-            colString = colString.slice(0, colString.length - 1);
-            db.exec(
-              `INSERT INTO ${bootstrapSanitized} (${colString}) VALUES (${valueString});`
-            );
+      ).then(() => {
+        //getting max number of partitions for entire broker (must have for SQL, as table columns cannot be altered after creating)
+        //so each topic will have the same number of partitions in SQL db, but null values in sqldb indicate the partition does not exist on that topic in kafka
+        // topic1 w/ 3 partitions (max for broker) -> {topic: topic1, partition_0: 5, partition_1: 2, partition_3: 0}
+        // topic2 w/ 1 partition (< max for broker) -> {topic: topic2, partition_0: 10, partition_1: null, partition_3: null}
+        let maxPartitions = 0;
+        Object.keys(offsets).forEach((el) => {
+          offsets[el].forEach((el2) => {
+            if (el2.partition > maxPartitions) maxPartitions = el2.partition;
           });
-        })
-        .then(() => {
-          admin.disconnect();
-          next();
         });
-    });
-  },
-  //grabs data from kafka admin for a specific topic, then updates it in sqldb, then reads sqldb and sends it to frontend
-  refresh: function (req, res) {
-    const { bootstrap, topic } = req.body;
-    const bootstrapSanitized = bootstrap.replace(':', '_');
-    const instance = new kafka.Kafka({
-      brokers: [`${bootstrap}`],
-      clientId: 'testing2', //hardcoded, probably should use username from state
-    });
-    const admin = instance.admin();
-    //below is query string used to update database, does not need to be ordered e.g. (partition_43 = 0, partition_0 = 17...)
-    let setString = '';
-    admin.connect();
-    admin
-      .fetchTopicOffsets(topic)
-      .then((result) => {
-        result.forEach((el) => {
-          //adding each partitions value to the set string
-          setString += `partition_${el.partition} = ${el.offset},`;
-        });
-      })
-      .then(() => {
-        //!!important!! slicing off last comma, which will throw a SQL syntax error
-        setString = setString.slice(0, setString.length - 1);
-        open({
-          filename: '/tmp/database.db',
-          driver: sqlite3.Database,
-        })
+        //creating partition string for SQL query, creates partition_X column in db
+        let partitionString = '';
+        for (let i = 0; i <= maxPartitions; i++) {
+          if (i < maxPartitions) partitionString += `partition_${i} int,`;
+          //below is for last value, which !!!cannot!!! have comma after it
+          else partitionString += `partition_${i} int`;
+        }
+        open({ filename: '/tmp/database.db', driver: sqlite3.Database })
           .then((db) => {
             db.exec(
-              `UPDATE ${bootstrapSanitized} SET ${setString} WHERE topic='${topic}';`
+              `CREATE TABLE ${bootstrapSanitized} (topic varchar(255), ${partitionString});`
             );
             return db;
           })
-          //here we grab topic data from sqldb (after updated)
           .then((db) => {
-            db.all(
-              `SELECT * FROM ${bootstrapSanitized} WHERE topic='${topic}'`
-            ).then((result) => {
-              //new arr which holds the correctly formated data for d3
-              const arr: { time: number; value: number }[] = [];
-              Object.keys(result[0]).forEach((el) => {
-                if (el !== 'topic') {
-                  arr.push({
-                    //slicing off first part of colname and turning into number (for d3) (partition_1 -> 1)
-                    time: Number(el.slice(10)),
-                    value: result[0][el],
-                  });
-                }
+            Object.keys(offsets).forEach((el) => {
+              //below is for generic column names e.g. topic, partition_0, partition_1...
+              let colString = 'topic, ';
+              //below is the actual values for those columns above(!!topic must be in quotes!!) e.g. 'intial_report', 10, 7
+              let valueString = `'${el}', `;
+              offsets[el].forEach((el2) => {
+                valueString += `'${el2.offset}',`;
+                colString += `partition_${el2.partition},`;
               });
-              res.status(200).json(arr);
+              valueString = valueString.slice(0, valueString.length - 1);
+              colString = colString.slice(0, colString.length - 1);
+              db.exec(
+                `INSERT INTO ${bootstrapSanitized} (${colString}) VALUES (${valueString});`
+              );
             });
+          })
+          .then(() => {
+            admin.disconnect();
+            next();
           });
       });
+    } catch (error) {
+      console.log(error);
+      next(error);
+    }
+  },
+  //grabs data from kafka admin for a specific topic, then updates it in sqldb, then reads sqldb and sends it to frontend
+  refresh: function (req, res, next) {
+    try {
+      const { bootstrap, topic } = req.body;
+      const bootstrapSanitized = bootstrap.replace(':', '_');
+      const instance = new kafka.Kafka({
+        brokers: [`${bootstrap}`],
+        clientId: 'testing2', //hardcoded, probably should use username from state
+      });
+      const admin = instance.admin();
+      //below is query string used to update database, does not need to be ordered e.g. (partition_43 = 0, partition_0 = 17...)
+      let setString = '';
+      admin.connect();
+      admin
+        .fetchTopicOffsets(topic)
+        .then((result) => {
+          result.forEach((el) => {
+            //adding each partitions value to the set string
+            setString += `partition_${el.partition} = ${el.offset},`;
+          });
+        })
+        .then(() => {
+          //!!important!! slicing off last comma, which will throw a SQL syntax error
+          setString = setString.slice(0, setString.length - 1);
+          open({
+            filename: '/tmp/database.db',
+            driver: sqlite3.Database,
+          })
+            .then((db) => {
+              db.exec(
+                `UPDATE ${bootstrapSanitized} SET ${setString} WHERE topic='${topic}';`
+              );
+              return db;
+            })
+            //here we grab topic data from sqldb (after updated)
+            .then((db) => {
+              db.all(
+                `SELECT * FROM ${bootstrapSanitized} WHERE topic='${topic}'`
+              ).then((result) => {
+                //new arr which holds the correctly formated data for d3
+                const arr: { time: number; value: number }[] = [];
+                Object.keys(result[0]).forEach((el) => {
+                  if (el !== 'topic') {
+                    arr.push({
+                      //slicing off first part of colname and turning into number (for d3) (partition_1 -> 1)
+                      time: Number(el.slice(10)),
+                      value: result[0][el],
+                    });
+                  }
+                });
+                res.status(200).json(arr);
+              });
+            });
+        });
+    } catch (error) {
+      console.log(error);
+      next(error);
+    }
   },
 };
 
