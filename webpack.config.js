9f38522cf606329174b60161154c0ce92df65873
adam thibodeaux <athib27@gmail.com>
2021-11-07 15:36:23 -0500

    updating webpack entrypoint

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   entry: {
-    app: './dist/index.js',
+    app: './dist/client/index.js',
   },
   output: {
     path: path.resolve(__dirname, 'build'),
