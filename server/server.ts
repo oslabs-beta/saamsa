cf8577de126f51b0546bf5bee6ffac898de7ac10
adam thibodeaux <athib27@gmail.com>
2021-11-08 10:08:53 -0500

    updating server to use https protcol

--- server/server.ts
+++ server/server.ts
@@ -2,7 +2,26 @@ import createServer from './createServer';
 import { connect, ConnectOptions } from 'mongoose';
 import { exec } from 'child_process';
 import * as path from 'path';
+import https from 'https';
+import fs from 'fs';
 const app = createServer();
+const privateKey = fs.readFileSync(
+  '/etc/letsencrypt/live/yourdomain.com/privkey.pem',
+  'utf8'
+);
+const certificate = fs.readFileSync(
+  '/etc/letsencrypt/live/yourdomain.com/cert.pem',
+  'utf8'
+);
+const ca = fs.readFileSync(
+  '/etc/letsencrypt/live/yourdomain.com/chain.pem',
+  'utf8'
+);
+const credentials = {
+  key: privateKey,
+  cert: certificate,
+  ca: ca,
+};
 
 const MONGO_URI =
   'mongodb+srv://dbUser:codesmith@cluster-saamsa.vys7y.mongodb.net/saamsa?retryWrites=true&w=majority';
@@ -13,9 +32,10 @@ connect(MONGO_URI, {
 } as ConnectOptions)
   .then(() => {
     console.log('Connected to MongoDB');
-    app.listen(3001, () => {
+    const httpsServer = https.createServer(credentials, app);
+    httpsServer.listen(443, () => {
       // exec(`electron ${path.join(__dirname, '../electron/index.js')}`);
-      console.log('server listening on port 3001 :)');
+      console.log('server listening on port 443 :)');
     });
   })
   .catch((err: Error) =>
